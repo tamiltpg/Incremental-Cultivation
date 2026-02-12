5058b299a23a2e8c581aa7c6cbef5e1d611ddc3d
Tamiltpg <144052374+tamiltpg@users.noreply.github.com>
2026-02-12 21:02:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Use the name of your GitHub repository here
+  base: "/Incremental-Cultivation/", 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
